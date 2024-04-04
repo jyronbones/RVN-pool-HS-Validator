c5d72fd7fc633179f3be8ce1d5eb065ada79fcf7
Byron <80455013+jyronbones@users.noreply.github.com>
2024-04-04 13:39:54 -0400

    implement scraping of relevent data

--- PoolScraper/scrape/scraper.py
+++ PoolScraper/scrape/scraper.py
@@ -1,24 +1,78 @@
-from configurations.driver_setup import WebDriverConfig
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from configurations.driver_setup import WebDriverConfig
+from settings import *
 
 
 class Scraper:
     def __init__(self, site_url):
         self.site_url = site_url
         self.driver = None
+        # Initialize all data lists
+        self.wallet_stats = []
+        self.earning_history = []
+        self.hash_rates = []
+        self.workers = []
 
     def start(self, proxy):
         self.driver = WebDriverConfig.configure_driver(proxy)
 
+    def get_element_text(self, xpath):
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
+        return element.text
+
+    def scrape_wallet_stats(self):
+        self.wallet_stats.append(self.get_element_text(WALLET_HASH_RATE))
+        self.wallet_stats.append(self.get_element_text(WALLET_ONLINE_WORKERS))
+        self.wallet_stats.append(self.get_element_text(WALLET_OFFLINE_WORKERS))
+        self.wallet_stats.append(self.get_element_text(WALLET_PAYOUT_COIN))
+        self.wallet_stats.append(self.get_element_text(WALLET_CLEARED_BALANCE))
+
+    def scrape_earning_history(self):
+        self.earning_history.append(self.get_element_text(EARNING_1_DAY))
+        self.earning_history.append(self.get_element_text(EARNING_7_DAYS))
+        self.earning_history.append(self.get_element_text(EARNING_14_DAYS))
+        self.earning_history.append(self.get_element_text(EARNING_30_DAYS))
+
+    def scrape_hash_rates(self):
+        self.hash_rates.append(self.get_element_text(HASH_RATE_5_MIN))
+        self.hash_rates.append(self.get_element_text(HASH_RATE_1_HOUR))
+        self.hash_rates.append(self.get_element_text(HASH_RATE_6_HOURS))
+        self.hash_rates.append(self.get_element_text(HASH_RATE_12_HOURS))
+        self.hash_rates.append(self.get_element_text(HASH_RATE_24_HOURS))
+
+    def scrape_workers(self):
+        # Assuming that you have the same number of attributes for each worker
+        for i in range(1, 4):  # Adjust the range if there are more workers
+            worker_stats = [
+                self.get_element_text(eval(f'WORKER_{i}_RIG_NAME')),
+                self.get_element_text(eval(f'WORKER_{i}_HS')),
+                self.get_element_text(eval(f'WORKER_{i}_DIFFICULTY')),
+                self.get_element_text(eval(f'WORKER_{i}_SPM')),
+                self.get_element_text(eval(f'WORKER_{i}_CONNECT_TIME')),
+                self.get_element_text(eval(f'WORKER_{i}_LAST_SEEN')),
+                self.get_element_text(eval(f'WORKER_{i}_LAST_SHARE')),
+                self.get_element_text(eval(f'WORKER_{i}_SERVER')),
+                self.get_element_text(eval(f'WORKER_{i}_TYPE')),
+            ]
+            self.workers.append(worker_stats)
+
     def scrape(self):
         print(f"Accessing {self.site_url}")
         self.driver.get(self.site_url)
-        # ... additional scraping logic ...
+        self.scrape_wallet_stats()
+        self.scrape_earning_history()
+        self.scrape_hash_rates()
+        self.scrape_workers()
 
     def run(self):
         try:
             self.scrape()
-            # Add any post-scraping actions (e.g., saving data)
+            # Here you would have the option to call another method to insert data into the database
         finally:
             time.sleep(10)  # Delay for observing the browser
             self.driver.quit()
--- PoolScraper/settings.py
+++ PoolScraper/settings.py
@@ -1,1 +1,60 @@
 SITE_URL = "https://www.ravenminer.com/ravencoin/wallet/REuaQ4hVkipRsu6tX27iENPrHxGpvfkFh9"
+
+# Scraper values Wallet Stats
+WALLET_HASH_RATE = "/html/body/main/div[4]/div[1]/div[1]/div[1]/div/div[1]/div"
+WALLET_ONLINE_WORKERS = "/html/body/main/div[4]/div[1]/div[1]/div[1]/div/div[2]/div/p[2]/span[1]"
+WALLET_OFFLINE_WORKERS = "/html/body/main/div[4]/div[1]/div[1]/div[1]/div/div[2]/div/p[2]/span[2]"
+WALLET_PAYOUT_COIN = "/html/body/main/div[4]/div[1]/div[1]/div[1]/div/div[3]/div/p[2]"
+WALLET_CLEARED_BALANCE = "/html/body/main/div[4]/div[1]/div[1]/div[1]/div/div[4]/div/p[2]/span/span[1]"
+
+# Earning History
+EARNING_1_DAY = ("/html/body/main/div[4]/div[1]/div[1]/div[3]/div[2]/div[1]/div/div/table/tbody/tr[1]/td["
+                   "2]/span/span[1]")
+EARNING_7_DAYS = ("/html/body/main/div[4]/div[1]/div[1]/div[3]/div[2]/div[1]/div/div/table/tbody/tr[2]/td["
+                      "2]/span/span[1]")
+EARNING_14_DAYS = ("/html/body/main/div[4]/div[1]/div[1]/div[3]/div[2]/div[1]/div/div/table/tbody/tr[3]/td["
+                         "2]/span/span[1]")
+EARNING_30_DAYS = ("/html/body/main/div[4]/div[1]/div[1]/div[3]/div[2]/div[1]/div/div/table/tbody/tr[4]/td["
+                       "2]/span/span[1]")
+
+# Wallet Hash-rate
+HASH_RATE_5_MIN = "/html/body/main/div[4]/div[1]/div[2]/div/div[2]/div[1]/div/div[3]/table/tbody/tr/td[1]/span[1]"
+HASH_RATE_1_HOUR = "/html/body/main/div[4]/div[1]/div[2]/div/div[2]/div[1]/div/div[3]/table/tbody/tr/td[2]/span[1]"
+HASH_RATE_6_HOURS = "/html/body/main/div[4]/div[1]/div[2]/div/div[2]/div[1]/div/div[3]/table/tbody/tr/td[3]/span[1]"
+HASH_RATE_12_HOURS = ("/html/body/main/div[4]/div[1]/div[2]/div/div[2]/div[1]/div/div[3]/table/tbody/tr/td["
+                          "4]/span[1]")
+HASH_RATE_24_HOURS = ("/html/body/main/div[4]/div[1]/div[2]/div/div[2]/div[1]/div/div[3]/table/tbody/tr/td["
+                              "5]/span[1]")
+
+# Workers
+WORKER_1_RIG_NAME = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[1]/td[2]"
+WORKER_1_HS = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[1]/td[3]/span[1]"
+WORKER_1_DIFFICULTY = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[1]/td[4]"
+WORKER_1_SPM = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[1]/td[5]"
+WORKER_1_CONNECT_TIME = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[1]/td[5]"
+WORKER_1_LAST_SEEN = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[2]/td[7]"
+WORKER_1_LAST_SHARE = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[1]/td[8]"
+WORKER_1_SERVER = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[1]/td[9]"
+WORKER_1_TYPE = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[1]/td[10]"
+
+WORKER_2_RIG_NAME = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[2]/td[2]"
+WORKER_2_HS = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[2]/td[3]/span[1]"
+WORKER_2_DIFFICULTY = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[2]/td[4]"
+WORKER_2_SPM = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[2]/td[5]"
+WORKER_2_CONNECT_TIME = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[2]/td[6]"
+WORKER_2_LAST_SEEN = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[2]/td[7]"
+WORKER_2_LAST_SHARE = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[2]/td[8]"
+WORKER_2_SERVER = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[2]/td[9]"
+WORKER_2_TYPE = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[2]/td[10]"
+
+WORKER_3_RIG_NAME = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[3]/td[2]"
+WORKER_3_HS = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[3]/td[3]/span[1]"
+WORKER_3_DIFFICULTY = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[3]/td[4]"
+WORKER_3_SPM = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[3]/td[5]"
+WORKER_3_CONNECT_TIME = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[3]/td[6]"
+WORKER_3_LAST_SEEN = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[3]/td[7]"
+WORKER_3_LAST_SHARE = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[3]/td[8]"
+WORKER_3_SERVER = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[3]/td[9]"
+WORKER_3_TYPE = "/html/body/main/div[4]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[3]/td[10]"
+
+# TODO: Payouts
